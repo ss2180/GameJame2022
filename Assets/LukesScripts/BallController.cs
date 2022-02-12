b3a7dbd3f0e6e2cff92fcd4d73dcdb0afd5eecef
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2022-02-12 15:16:01 +0000

    Basic ball controller

---
+++ Assets/LukesScripts/BallController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float force = 5f;
+    public Rigidbody2D body;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+            Move();
+    }
+
+    void Move()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+        var direction = new Vector3(horizontal, vertical, 0f);
+
+        body.AddForce(direction * force);
+    }
+}
