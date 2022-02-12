d24849697827bc971ed62a1a4e7cdd4c61c3c9dd
Solomon Chamberlin <56757393+ss2180@users.noreply.github.com>
2022-02-12 17:48:17 +0000

    Made particles move to centre

--- Assets/Particle.cs
+++ Assets/Particle.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class Particle : MonoBehaviour
 {
     private SpriteRenderer sprite;
-    private GameObject parent;
+    private GameObject player;
+    private Rigidbody2D rb; 
 
     // Start is called before the first frame update
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
         sprite.color = Color.green;
+
+        rb = GetComponent<Rigidbody2D>();
+
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 force = player.transform.position - gameObject.transform.position;
+
+        rb.AddForce(force.normalized);
     }
 }
