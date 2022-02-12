bc6a4b37bbd88c677b78e7510c9b61754f5f5314
Solomon Chamberlin <56757393+ss2180@users.noreply.github.com>
2022-02-12 17:23:24 +0000

    Particle spawning added

---
+++ Assets/Particle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    private GameObject parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
